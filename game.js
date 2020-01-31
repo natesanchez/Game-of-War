ce30b0aec15c760ad931675bd63f5122802898cb
Nate_Sanchez <NSanchez@usa.com>
2020-01-30 23:13:57 -0500

    added shuffle

--- game.js
+++ game.js
@@ -1,13 +1,14 @@
 let cardSuits = ["spades","clubs","hearts","diamonds"];
 let cardValues = ["02","03","04","05","06","07","08","09","10","11","12","13","14"];
 let cardDeck = [];
-let playerOne = [];
-let playerTwo = [];
+let playerOneDeck = [];
+let playerTwoDeck = [];
 let warDeclared = false;
 let gameOver = false;
 
 
-//Create Deck
+
+//Deck Creation
 let createDeck = () => {
 	for (let i=0; i<cardValues.length; i++) {
 	for (let j=0; j<cardSuits.length; j++) {
@@ -17,3 +18,13 @@ let createDeck = () => {
 	}
 }
 //Create Deck End
+
+
+//Shuffle
+let shuffle = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+//End Shuffle
