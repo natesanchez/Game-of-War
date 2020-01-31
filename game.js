f07dc19805a48774134221ee85598cb81108fbce
Nate_Sanchez <NSanchez@usa.com>
2020-01-30 21:23:40 -0500

    created create deck functionality

--- game.js
+++ game.js
@@ -1,3 +1,19 @@
 let cardSuits = ["spades","clubs","hearts","diamonds"];
 let cardValues = ["02","03","04","05","06","07","08","09","10","11","12","13","14"];
 let cardDeck = [];
+let playerOne = [];
+let playerTwo = [];
+let warDeclared = false;
+let gameOver = false;
+
+
+//Create Deck
+let createDeck = () => {
+	for (let i=0; i<cardValues.length; i++) {
+	for (let j=0; j<cardSuits.length; j++) {
+		let cards = {value:cardValues[i], suit:cardSuits[j]}
+		cardDeck.push(cards);
+		}
+	}
+}
+//Create Deck End
